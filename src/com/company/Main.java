9270640ba1be6cbbdd4ec0f6171031c05d761225
Tema <My_Post_Box@bk.ru>
2020-04-22 19:48:58 +0300

    Поздравляем с написанием первой программы на Java!

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String hello="Hello";
+        String world="World";
+        System.out.println(hello+ " " + world + "!");
+
+    }
+}
